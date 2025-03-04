75d5b5c459c2d45f74f3eab597b91724c1407f4e
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-03-05 03:01:04 +0800

    chore: Removed useless files

--- src/main/java/parade/Main.java
+++ src/main/java/parade/Main.java
@@ -1,10 +1,8 @@
 package parade;
 
-import parade.utils.Utility;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println(Utility.Sum(5, 2));
     }
 }
--- src/main/java/parade/utils/Utility.java
+++
@@ -1,7 +0,0 @@
-package parade.utils;
-
-public class Utility {
-    public static int Sum(int a, int b) {
-        return a + b;
-    }
-}
