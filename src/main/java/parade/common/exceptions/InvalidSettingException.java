8f3962f08f1c5383dd7869a53f62c080d746cfc1
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-03-05 02:57:46 +0800

    feat: Added Settings class to initialise config

---
+++ src/main/java/parade/common/exceptions/InvalidSettingException.java
@@ -0,0 +1,19 @@
+package parade.common.exceptions;
+
+/**
+ * Exception thrown when an invalid setting configuration is encountered when building the Settings
+ * object.
+ */
+public class InvalidSettingException extends RuntimeException {
+    public InvalidSettingException(String message) {
+        super(message);
+    }
+
+    public InvalidSettingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidSettingException(Throwable cause) {
+        super(cause);
+    }
+}
