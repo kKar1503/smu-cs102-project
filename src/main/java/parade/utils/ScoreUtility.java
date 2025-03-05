6d773d4dbe65097c05e373fbce545af4e65d1235
Sri <srividya.rs.2024@computing.smu.edu.sg>
2025-03-05 14:15:25 +0800

    Basic_Game_Server and ScoreUtility

--- src/main/java/parade/utils/ScoreUtility.java
+++ src/main/java/parade/utils/ScoreUtility.java
@@ -1,3 +1,20 @@
 package parade.utils;
 
-public class ScoreUtility {}
+import java.util.List;
+import java.util.Map;
+
+import parade.common.Card;
+import parade.common.Colour;
+import parade.common.Player;
+
+public class ScoreUtility {
+    public static Map<Player, List<Colour>> decideMajority(Map<Player, List<Card>> playerCards) {
+        // TODO: to be implemented
+        throw new UnsupportedOperationException("unimplemented");
+    }
+
+    public static int calculateScore(List<Card> hand, List<Colour> majorityColours) {
+        // TODO: to be implemented
+        throw new UnsupportedOperationException("unimplemented");
+    }
+}
