ae192462da1baab861bb690efb5ba4e87176e31d
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-03-05 15:09:24 +0800

    feat: Added a main method that do a simple entry to the system

--- src/main/java/parade/Main.java
+++ src/main/java/parade/Main.java
@@ -1,8 +1,55 @@
 package parade;
 
+import parade.settings.SettingKey;
+import parade.settings.Settings;
+import parade.textrenderer.TextRendererProvider;
+import parade.textrenderer.impl.BasicTextRenderer;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Settings settings =
+                new Settings.Builder()
+                        .shouldValidateProperties(true)
+                        .fromClasspath("config.properties")
+                        .build();
+
+        String gameplayMode = settings.get(SettingKey.GAMEPLAY_MODE);
+
+        switch (gameplayMode) {
+            case "local":
+                TextRendererProvider.setInstance(new BasicTextRenderer());
+                break;
+            case "network":
+                throw new UnsupportedOperationException("Network mode is not yet supported");
+            default:
+                throw new IllegalStateException(
+                        "Unknown gameplay mode in settings: " + gameplayMode);
+        }
+
+        TextRendererProvider.getInstance().renderWelcome();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            TextRendererProvider.getInstance().renderMenu();
+            try {
+                int input = scanner.nextInt();
+                scanner.nextLine();
+                if (input == 2) {
+                    break;
+                }
+                startGame();
+            } catch (NoSuchElementException e) {
+                System.out.println("Invalid input, please try again.");
+            }
+        }
+
+        TextRendererProvider.getInstance().renderBye();
+    }
+
+    static void startGame() {
+        // TODO: implement game server logic
     }
 }
