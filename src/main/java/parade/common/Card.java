34144de1ac57f56f415940de73a1e7f2a7407e90
Riyaz Choudry <riyxz245@gmail.com>
2025-03-05 11:32:09 +0800

    Updated Card.java

--- src/main/java/parade/common/Card.java
+++ src/main/java/parade/common/Card.java
@@ -1,3 +1,24 @@
 package parade.common;
 
-public interface Card {}
+public class Card {
+
+    private final int number;
+    private final Colour colour;
+
+    public Card(int number, Colour colour) {
+        this.number = number;
+        this.colour = colour;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Colour getColour() {
+        return colour;
+    }
+
+    public String toString() {
+        return "Card{number=" + number + ", colour=" + colour + "}";
+    }
+}
