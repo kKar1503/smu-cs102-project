0a96841564ea03e315a6c433c21d0f8c3ba72ee2
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-03-10 23:24:14 +0800

    refactor: Changed Deck to use Stack instead of LinkedList and use overloaded draw method to support multi draw

--- src/main/java/parade/common/Deck.java
+++ src/main/java/parade/common/Deck.java
@@ -2,17 +2,15 @@ package parade.common;
 
 import parade.textrenderer.DebugRendererProvider;
 
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Deck {
 
-    private final LinkedList<Card> cards;
+    private final Stack<Card> cards;
 
     public Deck() {
-
-        cards = new LinkedList<>();
-
+        cards = new Stack<>();
+        cards.ensureCapacity(66); // We know it'll always be 6 (color) * 11 (0-10) cards
         for (Colour colour : Colour.values()) {
             for (int i = 0; i <= 10; i++) {
                 cards.add(new Card(i, colour));
@@ -31,12 +29,27 @@ public class Deck {
         return cards.isEmpty();
     }
 
-    public Card drawCard() {
-        if (!cards.isEmpty()) {
-            Card cardDrawn = cards.removeLast();
-            DebugRendererProvider.getInstance().debug("Drawn card: " + cardDrawn);
-            return cardDrawn;
+    public Card draw() {
+        if (cards.isEmpty()) {
+            DebugRendererProvider.getInstance().debug("Attempted to draw from an empty deck");
+            return null;
         }
-        return null;
+        Card drawnCard = cards.pop();
+        DebugRendererProvider.getInstance().debug("Drawn card: " + drawnCard);
+        return drawnCard;
+    }
+
+    public List<Card> draw(int count) {
+        List<Card> drawnCards = new ArrayList<>(count);
+        if (cards.isEmpty()) {
+            DebugRendererProvider.getInstance().debug("Attempted to draw from an empty deck");
+            return drawnCards;
+        }
+        for (int i = 0; i < count; i++) {
+            Card drawnCard = cards.pop();
+            DebugRendererProvider.getInstance().debug("Drawn card: " + drawnCard);
+            drawnCards.add(drawnCard);
+        }
+        return drawnCards;
     }
 }
