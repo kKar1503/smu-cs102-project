fb14dccc6fb49d530923c7f9449a61938ef212e1
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-03-19 16:24:36 +0800

    refactor: Interalised the setupLogger into LoggerProvider

--- src/main/java/parade/Main.java
+++ src/main/java/parade/Main.java
@@ -4,30 +4,26 @@ import parade.engine.AbstractGameEngine;
 import parade.engine.impl.LocalGameEngine;
 import parade.logger.AbstractLogger;
 import parade.logger.LoggerProvider;
-import parade.logger.impl.JsonLogger;
-import parade.logger.impl.MultiLogger;
-import parade.logger.impl.NopLogger;
-import parade.logger.impl.PrettyLogger;
 import parade.settings.SettingKey;
 import parade.settings.Settings;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args)
-            throws IOException, IllegalStateException, UnsupportedOperationException {
+            throws IOException,
+                    IllegalStateException,
+                    UnsupportedOperationException,
+                    InterruptedException {
 
         new Settings.Builder()
                 .shouldValidateProperties(true)
                 .fromClasspath("config.properties")
                 .build();
 
-        setupLogger();
+        LoggerProvider.setupLogger();
         Runtime.getRuntime()
                 .addShutdownHook(new Thread(() -> LoggerProvider.getInstance().close()));
         AbstractGameEngine gameEngine = setupGameEngine();
@@ -38,58 +34,6 @@ public class Main {
             LoggerProvider.getInstance().log("Error occurred during game execution", e);
             System.exit(1);
         }
-    }
-
-    private static void setupLogger() throws IOException {
-        Settings settings = Settings.getInstance();
-
-        String loggerTypes = settings.get(SettingKey.LOGGER_TYPES);
-        boolean shouldLog = settings.getBoolean(SettingKey.LOGGER_ENABLED);
-        AbstractLogger logger;
-        if (!shouldLog) {
-            logger = new NopLogger();
-        } else {
-            String[] loggerTypesArr = loggerTypes.split(",");
-            // Handles duplicate logger types and trim whitespace
-            List<String> loggerTypesArrNoDuplicates =
-                    Stream.of(loggerTypesArr).map(String::trim).distinct().toList();
-            if (loggerTypesArrNoDuplicates.size() == 1) {
-                logger = determineLoggerType(loggerTypesArrNoDuplicates.getFirst());
-            } else {
-                AbstractLogger[] loggers = new AbstractLogger[loggerTypesArrNoDuplicates.size()];
-                int i = 0;
-                for (String loggerType : loggerTypesArrNoDuplicates) {
-                    loggers[i++] = determineLoggerType(loggerType);
-                }
-                logger = new MultiLogger(loggers);
-            }
-        }
-        LoggerProvider.setInstance(logger);
-        logger.log("Initialised logger");
-    }
-
-    private static AbstractLogger determineLoggerType(String loggerType)
-            throws IllegalStateException, IOException {
-        return switch (loggerType) {
-            case "console" -> new PrettyLogger();
-            case "console_json" -> new JsonLogger(System.out);
-            case "file_json" -> {
-                String filePath = Settings.getInstance().get(SettingKey.LOGGER_FILE);
-                if (filePath == null || filePath.isEmpty()) {
-                    throw new IllegalStateException("File path for logger is not set in settings");
-                }
-                // Creates the directory if it does not exist
-                Path path = Path.of(filePath);
-                Path parentDir = path.getParent();
-                if (parentDir != null && !Files.exists(parentDir)) {
-                    Files.createDirectories(parentDir);
-                }
-                yield new JsonLogger(filePath);
-            }
-            default ->
-                    throw new IllegalStateException(
-                            "Unknown logger type in settings: " + loggerType);
-        };
     }
 
     /**
