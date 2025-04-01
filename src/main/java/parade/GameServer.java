5c019b797b2b21149f3c9233e5cc4d93de5b982c
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-04-01 22:40:18 +0800

    feat: Stripped useless stuff from the GameServer.java

--- src/main/java/parade/GameServer.java
+++ src/main/java/parade/GameServer.java
@@ -1,17 +1,12 @@
 package parade;
 
-import parade.controller.network.NetworkHumanPlayerController;
 import parade.logger.LoggerProvider;
 import parade.network.server.Server;
 import parade.settings.Settings;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GameServer {
-    static final List<NetworkHumanPlayerController> conn = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         new Settings.Builder()
                 .shouldValidateProperties(true)
