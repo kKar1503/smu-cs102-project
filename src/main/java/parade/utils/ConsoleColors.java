bfee306e70c893e72b9e26f7b8f339ca52497459
45tera <45terax@gmail.com>
2025-04-10 22:45:31 +0800

    fix: fix black colour

--- src/main/java/parade/utils/ConsoleColors.java
+++ src/main/java/parade/utils/ConsoleColors.java
@@ -5,7 +5,8 @@ public class ConsoleColors {
     public static final String RESET = "\033[0m"; // Text Reset
 
     // Regular Colors
-    public static final String BLACK = "\033[0;30m"; // BLACK
+    public static final String BLACK = "\033[0;90m"; // BLACK - SPECIAL  \033[0;30m
+
     public static final String RED = "\033[0;31m"; // RED
     public static final String GREEN = "\033[0;32m"; // GREEN
     public static final String YELLOW = "\033[0;33m"; // YELLOW
